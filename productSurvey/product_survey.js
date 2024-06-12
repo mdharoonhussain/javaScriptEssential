238f1cbe4675f24fa645c610529730589d2177cd
mdharoonhussain <haroonhussain97@gmail.com>
2024-06-12 04:28:52 -0400

    message

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,5 +1,4 @@
 function submitFeedback() {
-
     const username = document.getElementById('name').value;
     const age = document.getElementById('age').value;
     const email = document.getElementById('email').value;
@@ -7,9 +6,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
-     const submitButton=document.getElementById('submitBtn');
-     submitButton.onclick = submitFeedback;
-    alert('Thank you for your valuable feedback')
+
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
     document.getElementById('userEmail').innerHTML = email;
@@ -17,25 +14,17 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+
     document.getElementById('userInfo').style.display = 'block';
+    alert('Thank you for your valuable feedback')
+
 }
+const submitButton=document.getElementById('submitBtn');
 
-
-    // const username = document.getElementById('name').value;
-    // const age = document.getElementById('age').value;
-    // const email = document.getElementById('email').value;
-    // const job = document.getElementById('job').value;
-    // const designation = document.getElementById('designation').value;
-    // const productType = document.getElementById('productType').value;
-    // const feedback = document.getElementById('feedbackText').value;
-
-
-
-
+submitButton.onclick = submitFeedback;
 
 document.addEventListener('keydown', function(event) {
    if (event.key === 'Enter') {
      submitFeedback();
    }
  });
-
